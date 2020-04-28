eddb8d036d2e18c7787b2d76a0115e2247bb1b4f
hadyd <anr.hadyd@gmail.com>
2020-04-29 06:08:10 +0700

    First Init

---
+++ README.md
@@ -0,0 +1,4 @@
+## Program Mencari Angka Berbeda Dari Dua Array
+### Menggunakan Bahasa Pemrograman Go
+
+![Golang](https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1200px-Go_Logo_Blue.svg.png)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	array1 := []int{5, 6, 7, 90, 2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	array2 := []int{1, 12, 13, 14, 15, 16, 17, 18, 19, 20, 10, 20, 20, 50, 50, 60, 70, 80, 90, 7}
+
+	//Menemukan Perbedaan Dua Array:
+	result := difference(array1, array2)
+
+	fmt.Println("Array 1:", result)
+
+	result2 := difference(array2, array1)
+
+	fmt.Println("Array 2:", result2)
+
+}
+
+//Fungsi Untuk Membandingkan Dua Array
+func difference(a, b []int) []int {
+	//Key: Map Tipe Data: Int Value: Bool
+	target := map[int]bool{}
+	for _, x := range b {
+		target[x] = true
+	}
+
+	result := []int{}
+	for _, x := range a {
+		if _, ok := target[x]; !ok {
+			result = append(result, x)
+		}
+	}
+
+	return result
+}
